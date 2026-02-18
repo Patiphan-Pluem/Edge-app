be9c40061301be6621e977aff134c1fcfa56e353
Toikanongna <patiphan.td.p@gmail.com>
2026-02-18 19:51:27 +0700

    change pdc-id to 1410

--- TinyPDC.py
+++ TinyPDC.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
     
     target_ip = os.getenv("PMU_IP", "pmu-service.edge-apps")
     target_port = 1410
-    pdc = Pdc(pdc_id=7, pmu_ip=target_ip, pmu_port=target_port)
+    pdc = Pdc(pdc_id=1410, pmu_ip=target_ip, pmu_port=target_port)
 
     # Loop เชื่อมต่อ
     while True:
